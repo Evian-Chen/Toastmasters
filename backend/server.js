2d154eb11d87f1db68915d20c7428a3a519eefa3
Evian-Chen <paramecium128@gmail.com>
2025-06-05 19:34:04 +0800

    add mongo connection to server

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
 const userRouter = require("./routes/user.js");
 
 const app = express();
@@ -6,12 +9,18 @@ const portNum = 8888;
 
 // API design, endpoint (receive request from client)
 app.get("/", (req, res) => {
-    res.send("hi, this is node.js server");
+  res.send("hi, this is node.js server");
 });
 
 // 只要是從/user進來，都會被導到user.js這個物件裡面
 app.use("/user", userRouter);
 
-app.listen(portNum, () => {
-    console.log(`server is running at localhost: ${portNum}`);
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(portNum, () => {
+      console.log(`server is running at localhost: ${portNum}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`mongo connection error: ${error}`);
+  });
