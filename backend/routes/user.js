ab1d9d8cc72ab32fd0fe8b040361ca35c8e4f7e5
Evian-Chen <paramecium128@gmail.com>
2025-06-06 14:24:13 +0800

    practice promise

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,7 @@
 // setup routers
 const express = require("express");
+const fs = require("fs");
+
 const router = express.Router();
 
 // /user
@@ -7,11 +9,42 @@ router.get("/", (req, res) => {
     res.send("this is user");
 });
 
+// 使用promise讀取多個檔案
+let readFilePromise = (datapath) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(datapath, "utf-8", (err, data) => {
+            if (err) {
+                console.log(`error: ${err}`);
+                reject(JSON.parse(err));
+            } else {
+                resolve(JSON.parse(data));
+            }
+        });
+    })
+};
+
 // /user/me
-router.get("/me", (req, res) => {
-    res.json({
-        message: "this is user/me"
-    });
+router.get("/me", async (req, res) => {
+    // Promise.all([
+    //     readFilePromise("data.json"),
+    //     readFilePromise("cityDistrict.json")
+    // ])
+    // .then(result => {
+    //     res.json(result);
+    // })
+    // .catch(err => {
+    //     res.json(err);
+    // })
+
+    try {
+        let results = {};
+        results["data1"] = await readFilePromise("cityDistrict.json");
+        results["data2"] = await readFilePromise("data.json");
+        res.json(results);
+    } catch (err) {
+        res.json(err);
+    }
 });
+
 
 module.exports = router;
