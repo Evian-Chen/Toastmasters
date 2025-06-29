298a6cb51466f203413abeb6c9424c1a65f98065
Evian-Chen <paramecium128@gmail.com>
2025-06-29 19:27:34 +0800

    update package.json

--- oauth-test/client/src/main.js
+++ oauth-test/client/src/main.js
@@ -9,6 +9,9 @@ import router from './router'
 
 axios.defaults.withCredentials = true;
 
+// 設置 axios 全局基礎 URL
+axios.defaults.baseURL = import.meta.env.PROD ? '/api' : 'http://localhost:3000/api';
+
 const app = createApp(App)
 
 app.use(createPinia())
